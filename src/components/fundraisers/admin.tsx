bcb34785e9a4e32eeb5101da55a12d2300d20fe0
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2024-12-27 18:48:19 +0300

    feat: update admin management UI to handle adding and removing admins with form submission

--- src/components/fundraisers/admin.tsx
+++ src/components/fundraisers/admin.tsx
@@ -1,5 +1,5 @@
 import { addAdmin, removeAdmin } from "../../blockchain-services/useCharityDonation"
-import { useState } from "react"
+import React, { useState } from "react"
 
 export default function AdminManagement() {
   const [isAdding, setIsAdding] = useState<boolean>(false)
@@ -7,66 +7,114 @@ export default function AdminManagement() {
   const [formValue, setFormValue] = useState<{address: string}>({address : ''})
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-          const {name, value} = e.target;
-          setFormValue((prevValues) => ({
-              ...prevValues,
-              [name]: value,
-          }))
+    const {name, value} = e.target;
+    setFormValue((prevValues) => ({
+        ...prevValues,
+        [name]: value,
+    }))
   }
 
-  const handleAddAdmin = async () => {
+  const handleAddAdmin = async (e: React.FormEvent) => {
+    e.preventDefault()
+    
     setIsAdding(true)
     try {
-      await addAdmin('0xC099f8A2C5117C81652A506aFfE10a6E77e79808')
+      await addAdmin(formValue.address)
       setIsAdding(false)
     } catch (error) {
       console.error(`Error When Adding Admin ${error}`)
+    } finally {
+      setFormValue({address: ''})
+      setIsAdding(false)
     }
   }
 
-  const handleRemoveAdmin = async () => {
+  const handleRemoveAdmin = async (e: React.FormEvent) => {
+    e.preventDefault()
+    
     setIsRemoving(true)
     try {
       await removeAdmin('0xC099f8A2C5117C81652A506aFfE10a6E77e79808')
       setIsRemoving(false)
     } catch (error) {
       console.error(`Error When Removing Admin ${error}`)
+    } finally {
+      setFormValue({address: ''})
+      setIsRemoving(false)
     }
   }
 
   return (
-    <div className="bg-green-400 flex justify-center items-center p-2">
+    <div className="flex justify-center items-center p-2">
       <div className="md:w-1/2">
         <div className="join join-vertical w-full">
-          <div className="collapse collapse-arrow join-item border-base-300 border">
+          <div className="collapse collapse-arrow join-item border-green-700 border">
             <input type="radio" name="my-accordion-4" defaultChecked />
             <div className="collapse-title text-xl font-medium">Fundraiser Admins</div>
             <div className="collapse-content">
               <p>hello</p>
             </div>
           </div>
-          <div className="collapse collapse-arrow join-item border-base-300 border">
+          <div className="collapse collapse-arrow join-item border-green-700 border">
             <input type="radio" name="my-accordion-4" />
             <div className="collapse-title text-xl font-medium">Add Admin</div>
-            <form action="">
-              <label className="input input-bordered flex items-center justify-between gap-2 mb-1 font-semibold text-green-600">
-                    Title
+            <div className="collapse-content">
+              <form onSubmit={handleAddAdmin}>
+                <label className="input input-bordered flex items-center justify-between gap-2 mb-1 font-semibold text-green-600">
+                    Address
                     <input 
                         type="text" 
-                        id="title"
-                        name="title"
+                        id="address"
+                        name="address"
                         className="md:w-5/6 p-2 text-white" 
-                        placeholder="Fundraiser Name" 
+                        value={formValue.address}
+                        onChange={handleChange}
+                        placeholder="Admin's Address" 
                         required
                     />
-                </label>
-            </form>
+                  </label>
+                  <div className="p-1 flex justify-center">
+                    <button 
+                      type="submit" 
+                      className="btn bg-green-700 w-1/2 text-white text-base border border-green-700"
+                    >
+                      {
+                        isAdding ? 'Adding ...' : 'Add'
+                      }
+                    </button>
+                  </div>
+              </form>
+            </div>
           </div>
-          <div className="collapse collapse-arrow join-item border-base-300 border">
+          <div className="collapse collapse-arrow join-item border-green-700 border">
             <input type="radio" name="my-accordion-4" />
             <div className="collapse-title text-xl font-medium">Remove Admin</div>
             <div className="collapse-content">
-              <p>hello</p>
+              <form onSubmit={handleRemoveAdmin}>
+                <label className="input input-bordered flex items-center justify-between gap-2 mb-1 font-semibold text-green-600">
+                    Address
+                    <input 
+                        type="text" 
+                        id="address"
+                        name="address"
+                        className="md:w-5/6 p-2 text-white" 
+                        value={formValue.address}
+                        onChange={handleChange}
+                        placeholder="Admin's Address" 
+                        required
+                    />
+                  </label>
+                  <div className="p-1 flex justify-center">
+                    <button 
+                      type="submit" 
+                      className="btn bg-green-700 w-1/2 text-white text-base border border-green-700"
+                    >
+                      {
+                        isRemoving ? 'Removing ...' : 'Remove'
+                      }
+                    </button>
+                  </div>
+              </form>
             </div>
           </div>
         </div>
