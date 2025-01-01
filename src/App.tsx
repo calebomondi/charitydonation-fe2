fe6da0f9fda09d686e64c01a49ccbdc4458db7e9
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-01-01 23:31:27 +0300

    refactor: remove Web3BrowserCheck component and simplify App structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import MyRoutes from "./components/Routes/routes"
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
 
-import Web3BrowserCheck from "./components/Home/web3socket";
-
 function App() {
 
   return (
-    <Web3BrowserCheck>
+    <>
       <ToastContainer />
       <MyRoutes />
-    </Web3BrowserCheck>
+    </>
   )
 }
 
